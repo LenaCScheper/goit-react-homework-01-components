c4fe9ee55d27accc4462bf1f1a9c264464a48b6b
LenaCScheper <ven.lenysja@ukr.net>
2022-02-09 16:00:56 +0200

    добавила изменения

---
+++ build/precache-manifest.820fe3353e462739cb151001b2e14538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73625d6415b6f0425c512160e12fcf02",
+    "url": "/goit-react-homework-01-components/index.html"
+  },
+  {
+    "revision": "c709ffad5297669fbd96",
+    "url": "/goit-react-homework-01-components/static/css/main.20395e76.chunk.css"
+  },
+  {
+    "revision": "b4254a951c4e5f6e185c",
+    "url": "/goit-react-homework-01-components/static/js/2.8f027ee2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-homework-01-components/static/js/2.8f027ee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c709ffad5297669fbd96",
+    "url": "/goit-react-homework-01-components/static/js/main.2e8a4d30.chunk.js"
+  },
+  {
+    "revision": "195af1369a1123b19572",
+    "url": "/goit-react-homework-01-components/static/js/runtime-main.00c2243c.js"
+  }
+]);
